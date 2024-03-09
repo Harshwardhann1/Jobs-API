ef98b3c21a988cb1f8639ffef34c755f83426a18
Harsh Wardhan <wardhan071@gmail.com>
2024-03-09 20:13:01 +0530

    first commit

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,19 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
   res.send('get all jobs');
 };
 const getJob = async (req, res) => {
   res.send('get job');
 };
 const createJob = async (req, res) => {
-  res.send('create job');
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
   res.send('update job');
